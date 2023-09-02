b34a44b73dd68199b23bb6700bd78c74c05e1b2a
Ying-xi <yingxi1@student.unimelb.edu.au>
2023-09-02 16:32:09 +1000

    Fix code

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // 注意多了一个 Routes 的导入
-import Navbar from './navbar';
-import Homepage from './homepage';
-import MusicPlayer from './musicplayer';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
+import Navbar from './navbar'
+import Homepage from './homepage'
+import MusicPlayer from './musicplayer'
 
-
-function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Homepage />} />
-                    <Route path="/music" element={<MusicPlayer />} />
-                </Routes>
-                {/* <Dock /> */}
-            </div>
-        </Router>
-    );
+function App () {
+  return (
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/music' element={<MusicPlayer />} />
+        </Routes>
+        {/* <Dock /> */}
+      </div>
+    </Router>
+  )
 }
 
-export default App;
-
-
-
-// function App() {
-//   const [currentPage, setCurrentPage] = useState('homepage'); // 默认显示主页
-
-//   const switchToHomepage = () => {
-//     setCurrentPage('homepage');
-//   };
-
-//   const switchToMusicPlayer = () => {
-//     setCurrentPage('musicplayer');
-//   };
-
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       {currentPage === 'homepage' && <Homepage />}
-//       {currentPage === 'musicplayer' && <MusicPlayer />}
-//       <Dock />
-
-//       <div className="page-switch-buttons">
-//         <button onClick={switchToHomepage}>Switch to Homepage</button>
-//         <button onClick={switchToMusicPlayer}>Switch to Music Player</button>
-//       </div>
-//     </div>
-//   );
-// }
-
+export default App
