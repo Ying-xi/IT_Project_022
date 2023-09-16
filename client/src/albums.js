052ff3ecf8db6b665d65e74ff6ca7ca6b4ceb242
zhliu3 <zhliu3@student.unimelb.edu.au>
2023-09-16 15:00:34 +1000

    album page be transferred(with pixel)

--- client/src/albums.js
+++ client/src/albums.js
@@ -1,73 +1,105 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import './albums.css';
-//strange bug:
-function Albums() {
-    const [activeItem, setActiveItem] = useState(0);
 
-    useEffect(() => {
+class Albums extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeItemIndex: 0,
+            backgroundImage: '',
+        };
+    }
 
-    }, []);
+    componentDidMount() {
+        this.handleScroll();
+        window.addEventListener('scroll', this.handleScroll);
+    }
 
-    return (
-        <div className="shell" id="shell">
-            <div className="header">
-                <h2 className="title">Albums</h2>
-                <h3 className="subtitle">
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        const itemElements = document.querySelectorAll('.item');
+        const windowHeight = window.innerHeight;
+        const scrollPosition = window.scrollY;
+
+        itemElements.forEach((item, index) => {
+            const rect = item.getBoundingClientRect();
+            const itemCenter = rect.top + rect.height / 2;
+
+            if (itemCenter > windowHeight * 0.4 && itemCenter < windowHeight * 0.5) {
+                this.setState({ activeItemIndex: index });
+
+                // set the image of the item now to be the background
+                const imgElement = item.querySelector('.img');
+                if (imgElement) {
+                    const imageUrl = imgElement.getAttribute('src');
+                    this.setState({ backgroundImage: `url(${imageUrl})` });
+                }
+            }
+        });
+    };
+
+    render() {
+        const musicItems = [
+            {
+                type: 'Realxing Music',
+                title: 'Name',
+                imageUrl: '/albums/album1.jpg',
+                description:
+                    'Relaxing music often incorporates natural sounds like flowing water, birdsong, or ocean waves, enhancing the overall calming effect.',
+            },
+            {
+                type: 'White noise',
+                title: 'Name',
+                imageUrl: '/albums/album2.jpg',
+                description:
+                    'White noise is often used for its ability to mask other sounds, aiding in concentration, relaxation, or sleep by creating a consistent background noise.',
+            },
+            {
+                type: 'Jazz Music',
+                title: 'Name',
+                imageUrl: '/albums/album3.jpg',
+                description:
+                    "Jazz music's ability to blend diverse influences from different cultures and styles creates a rich tapestry of sound.",
+            },
+            {
+                type: 'Classic Music',
+                title: 'Name',
+                imageUrl: '/albums/album4.jpg',
+                description:
+                    'Classical music is a timeless genre known for its complexity, emotional depth, and intricate compositions, often performed by orchestras and chamber ensembles.',
+            },
+        ];
+
+        return (
+            <div className="shell" id="shell" style={{ backgroundImage: this.state.backgroundImage }}>
+                <div className="header">
+                    <h2 className="title">Albums</h2>
                     <h3 className="subtitle">click album cover <br /> to continue</h3>
-                </h3>
-            </div>
-            <div className="timeline">
-                <div className="item" data-text="Relaxing Music">
-                    <div className="content">
-                        <a href="https://example.com">
-                            <img className="img" src="./album1.jpg" alt="Album 1" />
-                        </a>
-                        <h2 className="content-title">Name</h2>
-                        <p className="content-desc">
-                            "Relaxing music often incorporates natural sounds like flowing water, birdsong, or ocean waves,
-                            enhancing the overall calming effect."
-                        </p>
-                    </div>
                 </div>
-                <div className="item" data-text="White noise">
-                    <div className="content">
-                        <a href="https://example.com">
-                            <img className="img" src="./album2.jpg" alt="Album 2" />
-                        </a>
-                        <h2 className="content-title">Name</h2>
-                        <p className="content-desc">
-                            "White noise is often used for its ability to mask other sounds, aiding in concentration,
-                            relaxation, or sleep by creating a consistent background noise."
-                        </p>
-                    </div>
-                </div>
-                <div className="item" data-text="Jazz Music">
-                    <div className="content">
-                        <a href="https://example.com">
-                            <img className="img" src="./album3.jpg" alt="Album 3" />
-                        </a>
-                        <h2 className="content-title">Name</h2>
-                        <p className="content-desc">
-                            "Jazz music's ability to blend diverse influences from different cultures and styles creates a
-                            rich tapestry of sound."
-                        </p>
-                    </div>
-                </div>
-                <div className="item" data-text="Classical Music">
-                    <div className="content">
-                        <a href="https://example.com">
-                            <img className="img" src="./album4.jpg" alt="Album 4" />
-                        </a>
-                        <h2 className="content-title">Name</h2>
-                        <p className="content-desc">
-                            "Classical music is a timeless genre known for its complexity, emotional depth, and intricate
-                            compositions, often performed by orchestras and chamber ensembles."
-                        </p>
-                    </div>
+                <div className="musiclist">
+                    {musicItems.map((item, index) => (
+                        <div
+                            key={index}
+                            className={`item ${this.state.activeItemIndex === index ? 'item--active' : ''
+                                }`}
+                            data-text={item.type}
+                        >
+                            <div className="content">
+                                <a href="https://example.com">
+                                    <img className="img" src={item.imageUrl} alt={item.type} />
+                                </a>
+                                <h2 className="content-title">{item.title}</h2>
+                                <p className="content-desc">{item.description}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Albums;
