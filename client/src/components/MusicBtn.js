c86099d46912336529ab196e6d49cfaba835d450
YUNFENG LIU <yunfengl1@student.unimelb.edu.au>
2023-09-24 04:40:04 +1000

    size change for music buttons

--- client/src/components/MusicBtn.js
+++ client/src/components/MusicBtn.js
@@ -5,8 +5,9 @@ const MusicBtn = (props) => {
   const face = process.env.PUBLIC_URL + props.face;
 
   return (
+    /* add color frames for the music buttons */
     <div className={`button music-btn-wrap ${props.color}`} onClick={props.onClick}>
-      <img src={face} alt="音乐按钮" className={`img-face`} />
+      <img src={face} alt="music button" className={`img-face`} />
       <p className="music-item-text">{props.name}</p>
     </div>
   );
