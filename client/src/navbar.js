150f482678bb1571121e70e6a689666609213fd9
YUNFENG LIU <yunfengl1@student.unimelb.edu.au>
2023-09-28 14:00:43 +1000

    search function

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
 
-function Navbar() {
+const SearchComp = () => {
   return (
-    <div className="navbar">
-      <div className="nav-links">
-        <Link to="/" className="nav-link">
-          🎵 Home
-        </Link>
-        <a href="/login" className="nav-link">
-          👤 Log in
-        </a>
-      </div>
-      <form className="search-container" action="/url" method="get">
-        {/* <img src={"../public/navbar-bg.jpg"} alt="" className="w-25px" /> */}
-        <input className="search-inp" type="text" placeholder="Search your music" />
-        <button type="submit" className="search-btn">
-          Search
-        </button>
-      </form>
+    <form className="search-container" action="/url" method="get">
+      {/* <img src={"../public/navbar-bg.jpg"} alt="" className="w-25px" /> */}
+      <input className="search-inp" type="text" placeholder="Search your music" />
+      <button type="submit" className="search-btn">
+        Search
+      </button>
+    </form>
+  );
+};
+
+const NavLinks = () => {
+  return (
+    <div className="nav-links">
+      <Link to="/" className="nav-link">
+        🎵 Home
+      </Link>
+      <a href="/login" className="nav-link">
+        👤 Log in
+      </a>
+    </div>
+  );
+};
+
+function Navbar() {
+  const route = useLocation();
+
+  return (
+    // <div className="navbar">{route.pathname === '/music' ? <SearchComp /> : <NavLinks />}</div>
+    <div className={route.pathname === '/music' ? 'gap navbar' : 'navbar'}>
+      <NavLinks />
     </div>
   );
 }
