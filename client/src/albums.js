11d36a14f4f3a0232dc610f481f848f4d82c3f40
zhliu3 <zhliu3@student.unimelb.edu.au>
2023-09-28 20:17:01 +1000

    solve the clash of playlist play, add glow title

--- client/src/albums.js
+++ client/src/albums.js
@@ -14,7 +14,7 @@ class Albums extends Component {
                     imageUrl: '/albums/album1.jpg',
                     lists: [
                         {
-                            musicName: 'Song 1',
+                            musicName: 'AuldLangSyne',
                             musicUrl: '/songs/AuldLangSyne.mp3'
                         },
                         {
@@ -78,50 +78,43 @@ class Albums extends Component {
     playMusic = (musicIndex, songIndex) => {
         const { musicItems } = this.state;
         const updatedMusicItems = [...musicItems]; // 创建音乐项的副本
-
+    
         // 获取当前音频元素
         const audio = this.audioRef;
-
-        // 如果正在播放的音乐与当前点击的音乐相同
-        if (this.state.currentMusicIndex === musicIndex) {
-            // 如果音频已暂停，则播放它
-            if (audio.paused) {
-                audio.play();
-            } else {
-                // 否则，暂停它
-                audio.pause();
-            }
-
-            // 更新歌曲的播放状态
-            updatedMusicItems[musicIndex].lists[songIndex].isPlaying = !audio.paused;
-        } else {
-            // 如果有其他音乐在播放，首先暂停它
-            if (this.audioRef) {
-                this.audioRef.pause();
-            }
-
-            // 设置新的音频源并播放
-            audio.src = musicItems[musicIndex].lists[songIndex].musicUrl;
-            audio.play();
-
-            // 添加音频结束事件监听器
-            audio.addEventListener('ended', () => {
-                this.setState({ currentMusicIndex: null });
-
-                // 更新歌曲的播放状态
-                updatedMusicItems[musicIndex].lists[songIndex].isPlaying = false;
+    
+        // 如果当前有音乐正在播放
+        if (this.state.currentMusicIndex !== null) {
+            // 先暂停当前正在播放的音乐
+            audio.pause();
+    
+            // 更新当前正在播放的音乐的状态为未播放
+            updatedMusicItems[this.state.currentMusicIndex].lists.forEach((song) => {
+                song.isPlaying = false;
             });
-
-            // 更新当前播放的音乐索引
-            this.setState({ currentMusicIndex: musicIndex });
-
-            // 更新歌曲的播放状态
-            updatedMusicItems[musicIndex].lists[songIndex].isPlaying = true;
         }
-
+    
+        // 设置新的音频源并播放
+        audio.src = musicItems[musicIndex].lists[songIndex].musicUrl;
+        audio.play();
+    
+        // 添加音频结束事件监听器
+        audio.addEventListener('ended', () => {
+            this.setState({ currentMusicIndex: null });
+    
+            // 更新歌曲的播放状态
+            updatedMusicItems[musicIndex].lists[songIndex].isPlaying = false;
+        });
+    
+        // 更新当前播放的音乐索引
+        this.setState({ currentMusicIndex: musicIndex });
+    
+        // 更新新点击的音乐的播放状态为播放中
+        updatedMusicItems[musicIndex].lists[songIndex].isPlaying = true;
+    
         // 更新状态中的音乐项列表
         this.setState({ musicItems: updatedMusicItems });
     };
+    
 
     // when the user scolling on the screen:
     componentDidMount() {
@@ -180,7 +173,7 @@ class Albums extends Component {
     // print them on the page:
     render() {
         const { musicItems } = this.state;
-
+    
         return (
             <div className="shell" id="shell" style={{ backgroundImage: this.state.backgroundImage }}>
                 <div className="header">
@@ -207,7 +200,9 @@ class Albums extends Component {
                                             >
                                                 {song.isPlaying ? '⏸️' : '▶️'}
                                             </span>
-                                            {song.musicName}
+                                            <span className={`song-name ${song.isPlaying ? 'playing-song' : ''}`}>
+                                                {song.musicName}
+                                            </span>
                                             <br />
                                         </span>
                                     ))}
@@ -219,8 +214,8 @@ class Albums extends Component {
                 <audio ref={(ref) => (this.audioRef = ref)} controls></audio>
             </div>
         );
-
     }
+    
 }
 
 export default Albums;
