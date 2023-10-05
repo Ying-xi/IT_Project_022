63b167a9429a5d42d244ebd92475c492188da729
zhliu3 <zhliu3@student.unimelb.edu.au>
2023-10-05 21:25:36 +1100

    add comments page

--- client/src/comments.js
+++ client/src/comments.js
@@ -1,9 +1,7 @@
 // comments.js
-import React from 'react';
+import React, { useState } from 'react';
 import './comments.css';
 import { useParams } from 'react-router-dom';
-
-
 function Comments() {
 
     const { musicIndex } = useParams();
@@ -25,11 +23,22 @@ function Comments() {
             break;
     }
 
+    //send comments:
 
 
+    const [comment, setComment] = useState(''); // 用于存储用户输入的评论
 
+    // 处理用户输入的评论
+    const handleCommentChange = (e) => {
+        setComment(e.target.value);
+    };
 
-
+    // 处理发送按钮点击事件
+    const handleSendClick = () => {
+        // 在这里可以执行发送评论的操作，可以将评论发送到后端或执行其他逻辑
+        alert(`发送评论: ${comment}`);
+        setComment(''); // 清空输入框内容
+    };
 
     return (
         //background:
@@ -45,13 +54,32 @@ function Comments() {
             }}
         >
 
+
             <div className="comments-void" id="comments-void">
+                <div className='comments-window'>
+
+                    <h2>
+                        Comments
+                    </h2>
+
+                    <textarea
+                        className="comments-input"
+                        placeholder="输入你的评论..."
+                        value={comment}
+                        onChange={handleCommentChange}
+                        
+                    />
+                    <button className="comments-button" onClick={handleSendClick}>
+                        发送
+                    </button>
+                    
+                </div>
                 <div className="crop">
 
-                    
+
                     <ul id="card-list" style={{ '--count': 6 }}>
 
-                        
+
                         <li>
                             <div className="comments-cards ">
                                 <a href="">
@@ -149,15 +177,17 @@ function Comments() {
                                 </a>
                             </div>
                         </li>
-                        
+
 
 
                     </ul>
                     <div className="last-circle"></div>
                     <div className="second-circle"></div>
                 </div>
+
                 <div className="mask"></div>
                 <div className="center-circle"></div>
+
             </div>
         </div>
     );
