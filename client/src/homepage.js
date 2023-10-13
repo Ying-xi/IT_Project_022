345aa091bf3f7e86b3fef4e85056cfab366501f4
zhliu3 <zhliu3@student.unimelb.edu.au>
2023-10-13 18:54:47 +1100

    change the link of homepage button

--- client/src/homepage.js
+++ client/src/homepage.js
@@ -16,7 +16,7 @@ function Homepage() {
       <Link to='/keyboard' className='window'>
         <img src='/homepage/play_music1.jpg' alt='#'/>
       </Link>
-      <Link to='/comments/0' className='window'>
+      <Link to='/albums' className='window'>
         <img src='/homepage/playlist1.jpg' alt='#'/>
       </Link>
     </div>
