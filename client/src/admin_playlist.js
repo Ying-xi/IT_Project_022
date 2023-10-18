d26e4820e50039c6d755b79c239e944c708fe2ff
Ying-xi <yingxi1@student.unimelb.edu.au>
2023-10-18 23:50:53 +1100

    set a playlist option

--- client/src/admin_playlist.js
+++ client/src/admin_playlist.js
@@ -53,15 +53,15 @@ function Admin_Playlist() {
 
     // const options = [];
     const options = [
-        { label: "Option 1", value: "option1" },
-        { label: "Option 2", value: "option2" },
-        { label: "Option 3", value: "option3" },
-        { label: "Option 4", value: "option4" },
-        { label: "Option 5", value: "option5" },
-        { label: "Option 6", value: "option6" },
-        { label: "Option 7", value: "option7" },
-        { label: "Option 8", value: "option8" },
+        { label: "Auld Lang Syne", value: "Auld Lang Syne" },
+        { label: "Bleu", value: "Bleu" },
+        { label: "Canon in D", value: "Canon in D" },
+        { label: "Relaxing Rain", value: "Relaxing Rain" },
+        { label: "Vocalise, Op. 34 No. 14", value: "Vocalise, Op. 34 No. 14" },
+        { label: "What Makes You Beautiful", value: "What Makes You Beautiful" },
+        { label: "Winter Bokeh", value: "Winter Bokeh" },
     ];
+
 
     // Handle music/playlist click event
     const handlePlaylistClick = (playlistId) => {
