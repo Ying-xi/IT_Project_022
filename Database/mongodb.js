7e7b69e52b394629dc903dfd00d1c9fef217111b
Ying-xi <62348590+Ying-xi@users.noreply.github.com>
2023-11-12 23:50:38 +1100

    rebuild

--- Database/mongodb.js
+++ Database/mongodb.js
@@ -1,25 +1,18 @@
-const mongoose = require('mongoose');
-const Upload = require('./upload');
+const mongoose = require('mongoose')
+const Upload = require('./upload')
+// const mongodb = process.env.MONGODB_PWD
+const mongodb = 'mongodb+srv://admin:sVAV1RGC6xqrBEL2@cluster0.kkodvpg.mongodb.net/music_therapy?retryWrites=true&w=majority'
+mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true })
 
-// const mongodb = process.env.MONGODB_PWD;
-const mongodb = 'mongodb+srv://admin:sVAV1RGC6xqrBEL2@cluster0.kkodvpg.mongodb.net/music_therapy?retryWrites=true&w=majority';
+const db = mongoose.connection
 
-const poolSize = 10;
 
-mongoose.connect(mongodb, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  poolSize: poolSize,
-});
+db.on('open', ()=>{
+    console.log('successful')
+})
 
-const db = mongoose.connection;
+db.on('error', ()=>{
+    console.log('error')
+})
 
-db.on('open', () => {
-  console.log('Connection successful');
-});
-
-db.on('error', (err) => {
-  console.error('Connection error:', err);
-});
-
-module.exports = db;
+module.exports = db
